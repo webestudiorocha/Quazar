99c99c65832a2163d60cfe48d03976cd0487bd4b
webestudiorocha <web@estudiorochayasoc.com.ar>
2019-01-11 12:25:29 -0300

    sdfdsf

--- blogs.php
+++ blogs.php
@@ -10,15 +10,31 @@ $template->set("favicon", LOGO);
 $template->themeInit();
 $novedades = new Clases\Novedades();
 $pagina = isset($_GET["pagina"]) ? $_GET["pagina"] : '0';
-$novedadesData = $novedades->listWithOps('', '', (3 * $pagina) . ',' . 3);
 $novedadesPaginador = $novedades->paginador('', 3);
 $imagenes = new Clases\Imagenes();
+$funciones = new Clases\PublicFunction();
+$pagina = isset($_GET["pagina"]) ? $_GET["pagina"] : '0';
 
-if(@count($_GET) == 0) {
-    $anidador = "?";
-} else {
-    $anidador = "&";
+$cantidad = 5;
+
+if ($pagina > 0) {
+    $pagina = $pagina - 1;
 }
+
+if (@count($_GET) > 1) {
+    $anidador = "&";
+} else {
+    $anidador = "?";
+}
+
+if (isset($_GET['pagina'])):
+    $url = $funciones->eliminar_get(CANONICAL, 'pagina');
+else:
+    $url = CANONICAL;
+endif;
+
+$novedadesData = $novedades->listWithOps("", "", $cantidad * $pagina . ',' . $cantidad);
+$numeroPaginas = $novedades->paginador("", $cantidad);
 ?>
 <!-- Start Banner Area -->
 <section class="banner-area organic-breadcrumb">
@@ -89,65 +105,54 @@ if(@count($_GET) == 0) {
         <div class="row">
             <div class="col-lg-8">
                 <div class="blog_left_sidebar">
+                    <?php
+                    foreach ($novedadesData as $nov) :
+                    $imagenes->set("cod", $nov['cod']);
+                    $img = $imagenes->view();
+                    $fecha = explode("-", $nov['fecha']);
+                    ?>
                     <article class="row blog_item">
+
                         <div class="col-md-3">
                             <div class="blog_info text-right">
-                                <div class="post_tag">
-                                    <a href="#">Food,</a>
-                                    <a class="active" href="#">Technology,</a>
-                                    <a href="#">Politics,</a>
-                                    <a href="#">Lifestyle</a>
-                                </div>
                                 <ul class="blog_meta list">
-                                    <li><a href="#">Mark wiens<i class="lnr lnr-user"></i></a></li>
-                                    <li><a href="#">12 Dec, 2018<i class="lnr lnr-calendar-full"></i></a></li>
-                                    <li><a href="#">1.2M Views<i class="lnr lnr-eye"></i></a></li>
-                                    <li><a href="#">06 Comments<i class="lnr lnr-bubble"></i></a></li>
+                                    <li><a href="#"> <span class="poster"><?php echo $fecha[2] . "/" . $fecha[1] . "/" . $fecha[0] ?></span><i class="lnr lnr-calendar-full"></i></a></li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-md-9">
                             <div class="blog_post">
-                                <img src="img/blog/main-blog/m-blog-1.jpg" alt="">
+                                <div class="project-photo"
+                                     style=" height: 300px; background: url(<?= URL . '/' . $img['ruta'] ?>) no-repeat center center/cover;">
+                                </div>
                                 <div class="blog_details">
-                                  <?php
-                                    foreach ($novedadesData as $nov) :
-                                        $imagenes->set("cod", $nov['cod']);
-                                        $img = $imagenes->view();
-                                        $fecha = explode("-", $nov['fecha']);
-                                        ?>
-
-                                    <a class="post-img" href="<?php echo URL . '/blogs/' . $funciones->normalizar_link($nov['titulo']) . "/" . $nov['id'] ?>"
-                                       style="height:440px;background:url(<?= URL . '/' . $img['ruta'] ?>)center/cover;">
-                                    </a>
-                                    <?php endforeach;?>
+                                    <a href="<?php echo URL . '/blog/' . $funciones->normalizar_link($nov['titulo']) . "/" . $nov['id'] ?>"><?= ucfirst($nov['titulo']) ?></a>
+                                    <p><?php echo strip_tags(substr($nov["desarrollo"],0,400)); ?>...</p>
                                     <a href="blog.php" class="white_bg_btn">View More</a>
                                 </div>
                             </div>
 
                         </div>
                     </article>
+                    <?php endforeach;?>
                     <nav class="blog-pagination justify-content-center d-flex">
                         <ul class="pagination">
-                            <li class="page-item">
-                                <a href="#" class="page-link" aria-label="Previous">
-                                        <span aria-hidden="true">
-                                            <span class="lnr lnr-chevron-left"></span>
-                                        </span>
-                                </a>
-                            </li>
-                            <li class="page-item"><a href="#" class="page-link">01</a></li>
-                            <li class="page-item active"><a href="#" class="page-link">02</a></li>
-                            <li class="page-item"><a href="#" class="page-link">03</a></li>
-                            <li class="page-item"><a href="#" class="page-link">04</a></li>
-                            <li class="page-item"><a href="#" class="page-link">09</a></li>
-                            <li class="page-item">
-                                <a href="#" class="page-link" aria-label="Next">
-                                        <span aria-hidden="true">
-                                            <span class="lnr lnr-chevron-right"></span>
-                                        </span>
-                                </a>
-                            </li>
+                            <li class="page-item page-link">
+                                <?php if (($pagina + 1) > 1): ?>
+                            <li><a href="<?= $url ?><?= $anidador ?>pagina=<?= $pagina ?>"><i
+                                            class="fa fa-angle-left" ></i></a></li>
+                            <?php endif; ?>
+
+                            <?php for ($i = 1; $i <= $numeroPaginas; $i++): ?>
+                                <li class="<?php if ($i == $pagina + 1) {
+                                    echo "active";
+                                } ?>"><a href="<?= $url ?><?= $anidador ?>pagina=<?= $i ?>"><?= $i ?></a></li>
+                            <?php endfor; ?>
+
+                            <?php if (($pagina + 2) <= $numeroPaginas): ?>
+                                <li><a href="<?= $url ?><?= $anidador ?>pagina=<?= ($pagina + 2) ?>"><i
+                                                class="fa fa-angle-right" ></i></a></li>
+                            <?php endif; ?>
                         </ul>
                     </nav>
                 </div>
